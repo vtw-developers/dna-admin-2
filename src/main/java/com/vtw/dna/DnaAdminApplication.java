eb488aa3cf2aa6fb189d3b8ce5c7da8354ce589a
우태진 <taejin.woo@vtw.co.kr>
2024-05-16 14:02:39 +0900

    Initial Commit

---
+++ src/main/java/com/vtw/dna/DnaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.vtw.dna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnaAdminApplication.class, args);
+    }
+
+}
