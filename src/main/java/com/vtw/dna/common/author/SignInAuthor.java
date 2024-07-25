89fa8db1858582ee86ab98690e0412b60ee0e174
김보경 <qhrud7127@naver.com>
2024-07-25 17:58:56 +0900

    조회 시 authorId jsonIgnore

--- src/main/java/com/vtw/dna/common/author/SignInAuthor.java
+++ src/main/java/com/vtw/dna/common/author/SignInAuthor.java
@@ -1,9 +1,11 @@
 package com.vtw.dna.common.author;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.vtw.dna.common.auth.dto.AuthUser;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public interface SignInAuthor {
+    @JsonIgnore
     default String getAuthorId() {
         AuthUser signInUser = (AuthUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return signInUser.getId();
