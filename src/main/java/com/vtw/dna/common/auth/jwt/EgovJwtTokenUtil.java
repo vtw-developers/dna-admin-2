573ac1aad3a3f30f1dbb6e673d481798dafb6c18
우태진 <taejin.woo@vtw.co.kr>
2024-08-30 15:43:59 +0900

    토큰 유효기간 변경

--- src/main/java/com/vtw/dna/common/auth/jwt/EgovJwtTokenUtil.java
+++ src/main/java/com/vtw/dna/common/auth/jwt/EgovJwtTokenUtil.java
@@ -20,7 +20,7 @@ public class EgovJwtTokenUtil {
 
     private static final long serialVersionUID = -5180902194184255251L;
     //public static final long JWT_TOKEN_VALIDITY = 24 * 60 * 60; //하루
-    public static final long JWT_TOKEN_VALIDITY = (long) ((1 * 60 * 60) / 60) * 60; //토큰의 유효시간 설정, 기본 60분
+    public static final long JWT_TOKEN_VALIDITY = (long) ((1 * 60 * 60) / 60) * 6000; //토큰의 유효시간 설정, 기본 60분
 
     public static final String SECRET_KEY = "egovframeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaegovframeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaegovframeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaegovframeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/*EgovProperties.getProperty("Globals.jwt.secret")*/;
 
